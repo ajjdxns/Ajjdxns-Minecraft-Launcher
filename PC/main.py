599f4d9630f1f3ca30f86d73416de404e2780d1d
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-02-21 08:01:03 +0000

    modified:   LICENSE 	modified:   PC/main.py 	new file:   PC/text/write.txt

--- PC/main.py
+++ PC/main.py
@@ -1,3 +1,32 @@
+'''
+Ajjdxns minecraft launcher
+(C) 2024 Ajjdxns
+'''
+
 import sys
+import os
 from console_start import console
-inputs = sys.argv
+
+command = ""
+argv = sys.argv
+
+try:
+    if argv[1] == '--version':
+        with open(r"/text/wirte.txt",'r') as f:
+            print(f.read())
+            os._exit()
+except:
+    os._exit(0)
+
+def main():
+    '''
+    主要的核心程序
+    '''
+    print("Ajjdxns minecraft launcher Copyright (C) 2024 Ajjdxns studio\n本程序没有任何保证，输入“license 15”了解更多。\n这是一个自由软件，欢迎再次分发。输入“license 3”了解详情。\n随程序应该有一份GPL3.0协议，如果没有找到，从这里找：https://www.gnu.org/licenses/。\n使用参数“--version”查看更多协议")
+    while True:
+        command = input("(AMLcommand)")
+        if command == "quit":
+            os._exit()
+
+if __name__ == "__main__":
+    main()
